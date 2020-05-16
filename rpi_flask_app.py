ffa50be64f51b2b8746356a884911347b10faa7f
jhawk7 <jhawk154@gmail.com>
2020-05-16 18:43:43 -0400

    adding healthcheck endpoint

--- rpi_flask_app.py
+++ rpi_flask_app.py
@@ -4,9 +4,15 @@ import os
 app = Flask(__name__)
 
 @app.route("/kill", methods=['POST']) #runs onn port 5000
-def kill_switch():
+def kill():
 	os.system("sudo shutdown now")
 	return '', 204
+
+
+@app.route("/healthcheck", methods=['GET'])
+def healthcheck():
+	return 'ok', 200
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000, debug=True)
